f9ef26deacf21201f82c3e8f0b68b11cd898524f
Francisco das Chagas <103084340+FranciscoChagasB@users.noreply.github.com>
2023-03-12 19:45:59 -0300

    Update

---
+++ AcessoDados/NiveisAcessoDados.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessoDados
+{
+    public class NiveisAcessoDados
+    {
+        SqlCommand comandoSql = new SqlCommand();
+        StringBuilder sql = new StringBuilder();
+        DataTable dadosTabela = new DataTable();
+
+        public void Salvar(string nome, string descricao)
+        //Método com o comando sql para salvar um nivel de usuário no banco de dados.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco.*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("INSERT INTO Nivel_Usuarios (NOME_NIVEL, DESCRICAO_NIVEL)");
+                    sql.Append(" VALUES (@nome, @descricao)");
+
+                    //configura os parâmetros que serão os dados inseridos na camada RegraNegocio.
+                    comandoSql.Parameters.Add(new SqlParameter("@nome", nome));
+                    comandoSql.Parameters.Add(new SqlParameter("@descricao", descricao));
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    comandoSql.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Salvar. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+
+        public void Alterar(int idNivel, string nome, string descricao)
+        //Método com o comando sql para alterar algum nível existente no banco de dados.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("UPDATE Nivel_Usuarios");
+                    sql.Append(" SET NOME_NIVEL = @nome, DESCRICAO_NIVEL = @descricao");
+                    sql.Append(" WHERE (ID_NIVEL = @idNivel)");
+
+                    //configura os parâmetros que serão os dados inseridos na camada RegraNegocio.
+                    comandoSql.Parameters.Add(new SqlParameter("@idNivel", idNivel));
+                    comandoSql.Parameters.Add(new SqlParameter("@nome", nome));
+                    comandoSql.Parameters.Add(new SqlParameter("@descricao", descricao));
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    comandoSql.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Alterar. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+
+        public void Excluir(int idNivel)
+        //Método com o comando sql para excluir algum nível existente no banco de dados.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("DELETE FROM Nivel_Usuarios");
+                    sql.Append(" WHERE (ID_NIVEL = @idNivel)");
+
+                    //configura os parâmetros que serão os dados inseridos na camada RegraNegocio.
+                    comandoSql.Parameters.Add(new SqlParameter("@idNivel", idNivel));
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    comandoSql.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Excluir. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+
+        public DataTable Listar()
+        //Método com o comando sql para listar os níveis existentes no banco de dados.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("SELECT * FROM Nivel_Usuario");
+                    sql.Append(" ORDER BY ID_NIVEL ASC");
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    dadosTabela.Load(comandoSql.ExecuteReader());
+                    return dadosTabela;
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Listar. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+    }
+}
---
+++ AcessoDados/UsuariosAcessoDados.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessoDados
+{
+    public class UsuariosAcessoDados
+    {
+        SqlCommand comandoSql = new SqlCommand();
+        StringBuilder sql = new StringBuilder();
+        DataTable dadosTabela = new DataTable();
+
+        public void Salvar(string nome, DateTime data, string login, int idNivel, string senha, string status)
+        //Método com o comando sql para salvar um usuário no banco de dados.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("INSERT INTO Usuarios (NOME_USUARIO, DATA_CADASTRO, LOGIN_USUARIO, SENHA_USUARIO, STATUS_USUARIO, ID_NIVEL)");
+                    sql.Append(" VALUES (@nome, @data, @login, @senha, @status, @idNivel)");
+
+                    //configura os parâmetros que serão os dados inseridos na camada RegraNegocio.
+                    comandoSql.Parameters.Add(new SqlParameter("@nome", nome));
+                    comandoSql.Parameters.Add(new SqlParameter("@data", data));
+                    comandoSql.Parameters.Add(new SqlParameter("@login", login));
+                    comandoSql.Parameters.Add(new SqlParameter("@senha", senha));
+                    comandoSql.Parameters.Add(new SqlParameter("@status", status));
+                    comandoSql.Parameters.Add(new SqlParameter("@idNivel", idNivel));
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    comandoSql.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Salvar. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+
+        public void Alterar(int idUsuario, string nome, DateTime data, string login, int idNivel, string senha, string status)
+        //Método com o comando sql para alterar algum usuráio existente no banco de dados.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("UPDATE Usuarios");
+                    sql.Append(" SET NOME_USUARIO = @nome, DATA_CADASTRO = @data, LOGIN_USUARIO = @login, SENHA_USUARIO = @senha, STATUS_USUARIO = @status, ID_NIVEL = @idNivel");
+                    sql.Append(" WHERE (ID_USUARIO = @idUsuario)");
+
+                    //configura os parâmetros que serão os dados inseridos na camada RegraNegocio.
+                    comandoSql.Parameters.Add(new SqlParameter("@idUsuario", idUsuario));
+                    comandoSql.Parameters.Add(new SqlParameter("@nome", nome));
+                    comandoSql.Parameters.Add(new SqlParameter("@data", data));
+                    comandoSql.Parameters.Add(new SqlParameter("@login", login));
+                    comandoSql.Parameters.Add(new SqlParameter("@senha", senha));
+                    comandoSql.Parameters.Add(new SqlParameter("@status", status));
+                    comandoSql.Parameters.Add(new SqlParameter("@idNivel", idNivel));
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    comandoSql.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Alterar. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+
+        public void Excluir(int idUsuario)
+        //Método com o comando sql para excluir algum usuário existente no banco de dados.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("DELETE FROM Usuarios");
+                    sql.Append(" WHERE (ID_USUARIO = @idUsuario)");
+
+                    //configura os parâmetros que serão os dados inseridos na camada RegraNegocio.
+                    comandoSql.Parameters.Add(new SqlParameter("@idUsuario", idUsuario));
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    comandoSql.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Excluir. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+
+        public DataTable Listar()
+        //Método com o comando sql para listar os usuários existentes no banco de dados.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("SELECT Usuarios.*, NOME_NIVEL FROM Usuarios INNER JOIN Nivel_Acesso");
+                    sql.Append(" ON Usuarios.ID_NIVEL = Nivel_Acesso.ID_NIVEL");
+                    sql.Append(" ORDER BY NOME_USUARIO ASC");
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    dadosTabela.Load(comandoSql.ExecuteReader());
+                    return dadosTabela;
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Listar. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+
+        public DataTable Login(string login, string senha)
+        //Método com o comando sql para selecionar e retornar o usuário que informamos o login e senha.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("SELECT * FROM Usuarios");
+                    sql.Append(" WHERE LOGIN_USUARIO = @login AND SENHA_USUARIO = @senha");
+
+                    //configura os parâmetros que serão os dados inseridos na camada RegraNegocio.
+                    comandoSql.Parameters.Add(new SqlParameter("@login", login));
+                    comandoSql.Parameters.Add(new SqlParameter("@senha", senha));
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    dadosTabela.Load(comandoSql.ExecuteReader());
+                    return dadosTabela;
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Login. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+
+        public DataTable RetornarLogin(string login)
+        //Método com o comando sql para selecionar e retornar o usuário que informamos o login.
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("SELECT * FROM Usuarios");
+                    sql.Append(" WHERE LOGIN_USUARIO = @login");
+
+                    //configura os parâmetros que serão os dados inseridos na camada RegraNegocio.
+                    comandoSql.Parameters.Add(new SqlParameter("@login", login));
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    dadosTabela.Load(comandoSql.ExecuteReader());
+                    return dadosTabela;
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método Listar. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+
+        public DataTable RetornarUsuario(int idUsuario)
+        /*Método com o comando sql que realiza a junção das tabelas Nível_Usuario e Usuarios
+         *para selecionar e retornar os dados do usuário que informamos o ID.
+        */
+        {
+            try
+            {
+                using (SqlConnection conexao = new SqlConnection(/*string de conexão com o banco*/))
+                {
+                    //inicia a conexao com o banco de dados.
+                    conexao.Open();
+
+                    //cria um texto com os comandos a serem inseridos no script.
+                    sql.Append("SELECT Usuarios.*, NOME_NIVEL FROM Usuarios INNER JOIN Nivel_Acesso");
+                    sql.Append(" ON Usuarios.ID_NIVEL = Nivel_Acesso.ID_NIVEL");
+                    sql.Append(" WHERE ID_USUARIO = @idUsuario");
+
+                    //configura os parâmetros que serão os dados inseridos na camada RegraNegocio.
+                    comandoSql.Parameters.Add(new SqlParameter("@idUsuario", idUsuario));
+
+                    //cria o script com o os comandos inseridos no texto "sql.Append" e o executa.
+                    comandoSql.CommandText = sql.ToString();
+                    comandoSql.Connection = conexao;
+                    dadosTabela.Load(comandoSql.ExecuteReader());
+                    return dadosTabela;
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro no método RetornarUsuario. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+            }
+        }
+    }
+}
