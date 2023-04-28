276d4a6184df62a0d26ac47c36872a9d8436981b
Francisco das Chagas <franciscobesilva@gmail.com>
2023-04-28 15:04:53 -0300

    Update

--- Interface/frmCategoria.cs
+++ Interface/frmCategoria.cs
@@ -12,9 +12,298 @@ namespace Interface
 {
     public partial class frmCategoria : Form
     {
+        private Panel panel1;
+        private Button btnAjuda;
+        private Button btnSair;
+        private Button btnExcluir;
+        private Button btnAlterar;
+        private DataGridView dtgNiveis;
+        private DataGridViewTextBoxColumn ID_CATEGORIA;
+        private DataGridViewTextBoxColumn NOME_CATEGORIA;
+        private DataGridViewTextBoxColumn DESCRICAO_CATEGORIA;
+        private Button btnLimpar;
+        private Button btnSalvar;
+        private TextBox txtDescricao;
+        private Label label3;
+        private TextBox txtNome;
+        private Label label2;
+        private Label label1;
+        private TextBox txtRegistro;
+        private Button btnNovo;
+
         public frmCategoria()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCategoria));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAjuda = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.dtgNiveis = new System.Windows.Forms.DataGridView();
+            this.ID_CATEGORIA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NOME_CATEGORIA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DESCRICAO_CATEGORIA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRegistro = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgNiveis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnAjuda);
+            this.panel1.Controls.Add(this.btnSair);
+            this.panel1.Controls.Add(this.btnExcluir);
+            this.panel1.Controls.Add(this.btnAlterar);
+            this.panel1.Controls.Add(this.btnNovo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(197, 444);
+            this.panel1.TabIndex = 5;
+            // 
+            // btnAjuda
+            // 
+            this.btnAjuda.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnAjuda.Image = global::Interface.Properties.Resources.ajuda;
+            this.btnAjuda.Location = new System.Drawing.Point(0, 294);
+            this.btnAjuda.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAjuda.Name = "btnAjuda";
+            this.btnAjuda.Size = new System.Drawing.Size(195, 74);
+            this.btnAjuda.TabIndex = 4;
+            this.btnAjuda.Text = "Ajuda";
+            this.btnAjuda.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAjuda.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSair.Image = global::Interface.Properties.Resources.sair;
+            this.btnSair.Location = new System.Drawing.Point(0, 368);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(195, 74);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnExcluir.Image = global::Interface.Properties.Resources.excluir;
+            this.btnExcluir.Location = new System.Drawing.Point(0, 148);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(195, 74);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Excluir Categoria";
+            this.btnExcluir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAlterar.Image = global::Interface.Properties.Resources.editar;
+            this.btnAlterar.Location = new System.Drawing.Point(0, 74);
+            this.btnAlterar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(195, 74);
+            this.btnAlterar.TabIndex = 1;
+            this.btnAlterar.Text = "Alterar Categoria";
+            this.btnAlterar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNovo.Image = global::Interface.Properties.Resources.adicionar;
+            this.btnNovo.Location = new System.Drawing.Point(0, 0);
+            this.btnNovo.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(195, 74);
+            this.btnNovo.TabIndex = 0;
+            this.btnNovo.Text = "Nova Categoria";
+            this.btnNovo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // dtgNiveis
+            // 
+            this.dtgNiveis.AllowUserToAddRows = false;
+            this.dtgNiveis.AllowUserToDeleteRows = false;
+            this.dtgNiveis.AllowUserToResizeColumns = false;
+            this.dtgNiveis.AllowUserToResizeRows = false;
+            this.dtgNiveis.BackgroundColor = System.Drawing.Color.White;
+            this.dtgNiveis.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgNiveis.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID_CATEGORIA,
+            this.NOME_CATEGORIA,
+            this.DESCRICAO_CATEGORIA});
+            this.dtgNiveis.Location = new System.Drawing.Point(209, 204);
+            this.dtgNiveis.Margin = new System.Windows.Forms.Padding(4);
+            this.dtgNiveis.Name = "dtgNiveis";
+            this.dtgNiveis.ReadOnly = true;
+            this.dtgNiveis.RowHeadersWidth = 51;
+            this.dtgNiveis.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgNiveis.Size = new System.Drawing.Size(609, 224);
+            this.dtgNiveis.TabIndex = 227;
+            // 
+            // ID_CATEGORIA
+            // 
+            this.ID_CATEGORIA.DataPropertyName = "ID_CATEGORIA";
+            this.ID_CATEGORIA.HeaderText = "Código";
+            this.ID_CATEGORIA.MinimumWidth = 6;
+            this.ID_CATEGORIA.Name = "ID_CATEGORIA";
+            this.ID_CATEGORIA.ReadOnly = true;
+            this.ID_CATEGORIA.Width = 65;
+            // 
+            // NOME_CATEGORIA
+            // 
+            this.NOME_CATEGORIA.DataPropertyName = "NOME_CATEGORIA";
+            this.NOME_CATEGORIA.HeaderText = "Nome";
+            this.NOME_CATEGORIA.MinimumWidth = 6;
+            this.NOME_CATEGORIA.Name = "NOME_CATEGORIA";
+            this.NOME_CATEGORIA.ReadOnly = true;
+            this.NOME_CATEGORIA.Width = 130;
+            // 
+            // DESCRICAO_CATEGORIA
+            // 
+            this.DESCRICAO_CATEGORIA.DataPropertyName = "DESCRICAO_CATEGORIA";
+            this.DESCRICAO_CATEGORIA.HeaderText = "Descrição";
+            this.DESCRICAO_CATEGORIA.MinimumWidth = 6;
+            this.DESCRICAO_CATEGORIA.Name = "DESCRICAO_CATEGORIA";
+            this.DESCRICAO_CATEGORIA.ReadOnly = true;
+            this.DESCRICAO_CATEGORIA.Width = 215;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnLimpar.Location = new System.Drawing.Point(831, 144);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(199, 31);
+            this.btnLimpar.TabIndex = 225;
+            this.btnLimpar.Text = "Limpar Campos";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.btnSalvar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSalvar.Location = new System.Drawing.Point(831, 101);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(199, 31);
+            this.btnSalvar.TabIndex = 226;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescricao.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtDescricao.Location = new System.Drawing.Point(209, 89);
+            this.txtDescricao.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDescricao.MaxLength = 500;
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDescricao.Size = new System.Drawing.Size(612, 104);
+            this.txtDescricao.TabIndex = 219;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(209, 68);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 224;
+            this.label3.Text = "Descrição:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.txtNome.Location = new System.Drawing.Point(393, 29);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(428, 29);
+            this.txtNome.TabIndex = 223;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 222;
+            this.label2.Text = "Nome:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(205, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 19);
+            this.label1.TabIndex = 220;
+            this.label1.Text = "Registro:";
+            // 
+            // txtRegistro
+            // 
+            this.txtRegistro.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRegistro.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRegistro.Location = new System.Drawing.Point(209, 29);
+            this.txtRegistro.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRegistro.Name = "txtRegistro";
+            this.txtRegistro.ReadOnly = true;
+            this.txtRegistro.Size = new System.Drawing.Size(118, 29);
+            this.txtRegistro.TabIndex = 221;
+            this.txtRegistro.TabStop = false;
+            this.txtRegistro.Text = "0";
+            // 
+            // frmCategoria
+            // 
+            this.ClientSize = new System.Drawing.Size(1045, 444);
+            this.Controls.Add(this.dtgNiveis);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRegistro);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmCategoria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerenciador de Categoria";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgNiveis)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
     }
 }
